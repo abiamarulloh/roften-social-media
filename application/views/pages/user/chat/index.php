3a58a872775176695a1a2d9d5a5771ed194d7c9b
abi <abiamarulloh06@gmail.com>
2021-01-26 23:19:08 +0800

    update

--- application/views/pages/user/chat/index.php
+++ application/views/pages/user/chat/index.php
@@ -1,3 +1,3 @@
-<div class="container" id="userChatList" style="margin-top: 80px;">
+<div class="container" id="userChatList" style="margin-top: 80px; overflow-y: scroll">
 	<input type="text" hidden value="<?= $user['id']; ?>" id="userIdOnChat">
 </div>
